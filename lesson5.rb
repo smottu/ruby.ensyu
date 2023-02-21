89c9776cdb9ff5828e9f8bf21f65f56838db8220
smottu <106876134+smottu@users.noreply.github.com>
2023-02-21 14:11:08 +0000

    lesson5

---
+++ lesson5.rb
@@ -0,0 +1,21 @@
+total_price = 100
+
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円。"
+elsif total_price < 100
+  puts "みかんを購入することができません。"
+end
+
+
+# 解答
+total_price = 100  # total_priceの値は任意です。
+
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円。"
+else
+  puts "みかんを購入することができません。"
+end
