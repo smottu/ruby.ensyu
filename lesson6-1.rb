0dcb268a8ee8939027c752aa56691e85f8362346
smottu <106876134+smottu@users.noreply.github.com>
2023-02-22 10:52:38 +0000

    lesson-6-1

---
+++ lesson6-1.rb
@@ -0,0 +1,18 @@
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+  input_key = gets
+  puts = "入力差sれ多値は#{input_key}"
+  puts = 
+puts "計算結果を出力します"
+
+
+# 解答
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+puts "計算結果を出力します"
+puts "#{a}*#{b}=#{a * b}"
+puts "計算を終了します"
